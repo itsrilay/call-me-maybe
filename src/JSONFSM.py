e457e000846231ea1f3377b1da7fdcc6a7f73116
Rui Silva <ruisilva@c2r1s7.42porto.com>
2026-04-06 16:00:48 +0100

    refactor: move StatesEnum to common structures for reusability

--- src/JSONFSM.py
+++ src/JSONFSM.py
@@ -1,24 +1,5 @@
-from enum import Enum, auto
 from src.models import FunctionDefinition
-
-
-class StrEnum(str, Enum):
-    """Base class where auto() assigns the name of the member as its value."""
-
-    @staticmethod
-    def _generate_next_value_(name, start, count, last_values):
-        return name
-
-
-class StatesEnum(StrEnum):
-    START = auto()
-    NAME_KEY = auto()
-    NAME_VALUE = auto()
-    ARGS_KEY = auto()
-    ARGS_START = auto()
-    PARAM_KEY = auto()
-    PARAM_VALUE = auto()
-    JSON_END = auto()
+from src.common import StatesEnum
 
 
 class JSONFSM:
